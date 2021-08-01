c15ec6784a0797c34c0d544d1ac1219ca2c5e20d
poojaml2 <73947275+poojaml2@users.noreply.github.com>
2021-08-01 15:12:15 +0530

    Add files via upload

---
+++ madlib.py
@@ -0,0 +1,8 @@
+adj = input("adjective:")
+noun = input("noun:")
+verb = input("verb:")
+adverb = input("adverb:")
+adj1 = input("adjective:")
+noun1 = input("noun:")
+madlib = f"today i went to the zoo. i saw a(n) {adj} {noun} jumping up and down in its tree.he {verb} {adverb} through the large tunnel that led to its {adj1} {noun1}."
+print(madlib)
